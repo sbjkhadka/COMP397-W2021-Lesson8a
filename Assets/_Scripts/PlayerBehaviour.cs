912d85aaaf2caa58d7bcbb43d7b958a8b6031bbd
Subarna Bijaya Khadka <khadka.subarnabijaya@gmail.com>
2021-03-09 04:04:30 -0500

    Implemented buttons for jump, minimap and control panel toggle

--- Assets/_Scripts/PlayerBehaviour.cs
+++ Assets/_Scripts/PlayerBehaviour.cs
@@ -72,27 +72,37 @@ public class PlayerBehaviour : MonoBehaviour
 
         controller.Move(move * maxSpeed * Time.deltaTime);
 
-        if (Input.GetButton("Jump") && isGrounded)
-        {
-            velocity.y = Mathf.Sqrt(jumpHeight * -2.0f * gravity);
-            jumpSound.Play();
-        }
+        //if (Input.GetButton("Jump") && isGrounded)
+        //{
+        //    Jump();
+        //}
 
         velocity.y += gravity * Time.deltaTime;
 
         controller.Move(velocity * Time.deltaTime);
 
-        if (Input.GetKeyDown(KeyCode.M))
-        {
-            // toggle the MiniMap on/off
-            miniMap.SetActive(!miniMap.activeInHierarchy);
-        }
+        //if (Input.GetKeyDown(KeyCode.M))
+        //{
+        //    ToggleMiniMap();
+        //}
     }
 
     void OnDrawGizmos()
     {
         Gizmos.color = Color.white;
         Gizmos.DrawWireSphere(groundCheck.position, groundRadius);
+    }
+
+    void Jump()
+    {
+        velocity.y = Mathf.Sqrt(jumpHeight * -2.0f * gravity);
+        jumpSound.Play();
+    }
+
+    void ToggleMiniMap()
+    {
+        // toggle the MiniMap on/off
+        miniMap.SetActive(!miniMap.activeInHierarchy);
     }
 
     public void TakeDamage(int damage)
@@ -104,6 +114,19 @@ public class PlayerBehaviour : MonoBehaviour
         {
             health = 0;
         }
+    }
+
+    public void onJumpButtonPressed()
+    {
+        if (isGrounded)
+        {
+            Jump();
+        }
+    }
+
+    public void onMapButtonPressed()
+    {
+        ToggleMiniMap();
     }
 
    
